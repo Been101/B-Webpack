8919cddc3a7fc990b551d4cc34c5297b58f358f5
been <1043609130@qq.com>
2019-12-05 22:44:30 +0800

    add filePlugin

--- src/demo6/plugins/FilePlugin.js
+++ src/demo6/plugins/FilePlugin.js
@@ -1,7 +1,29 @@
 // 生成一个md文件，里面放着所有的文件名
 
 class FilesPlugin {
+  constructor(options) {
+    this.options = options
+  }
   apply(compiler) {
-    compiler.hooks.emit.tapAsync()
+    compiler.hooks.emit.tapAsync('EmitPlugin', (compilation, callback) => {
+      console.log(this.options)
+      console.log(compilation.assets)
+      let content = '## 文件内容列表 \n'
+      for (let filename in compilation.assets) {
+        content += ` - ${filename} \n`
+      }
+
+      compilation.assets[this.options.filename || 'filelist.md'] = {
+        source() {
+          return content
+        },
+        size() {
+          return Buffer.byteLength(content)
+        }
+      }
+      callback()
+    })
   }
 }
+
+module.exports = FilesPlugin
